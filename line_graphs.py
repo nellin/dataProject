2e4b89dd451fdca110d8b51299406f541424b145
adrian-thumbtack <adrthamburaj@gmail.com>
2018-05-02 14:05:22 -0400

    rip copies
    the copies of a game from console to console have been oofed

--- line_graphs.py
+++ line_graphs.py
@@ -8,10 +8,29 @@ def create_line_graph(ser):
     ser : list
         contains the names of series (as strings) that will be represented on
         the line graph
+        
+        ex: ['Super Mario Galaxy', 'Fifa', 'Halo']
     '''
     datums = []
     for series in ser:
-        datums.append([video_game for video_game in video_games if series in video_game[0].lower()])
+        datums.append([video_game for video_game in video_games if series.lower() in video_game[0].lower()])
+    
+    print datums
+    print("\n")
+    
+    for datum in datums:
+        titles = [title[0] for title in datum]
+        i = len(titles)-1
+        while i>=0:
+            temp = titles.pop()
+            if temp in titles:
+                datum[titles.index(temp)][1] += datum[i][1]
+                datum[titles.index(temp)][1] = round(datum[titles.index(temp)][1], 2)
+                del datum[i]
+            i -= 1
+            
+    print datums
+            
 
 
 f = open('videogamesales/vgsales.csv', 'r')
@@ -30,7 +49,7 @@ for line in f.readlines():
     try:
         year = int(data[3])
     except ValueError:
-        year = 'N/A'
+        year = None
     
     video_games.append([title, global_sales, year])
      
