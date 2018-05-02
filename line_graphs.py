f2f3ec010b837c4665e3b026876c6a197fc6a269
svshmn <sivanshulman@gmail.com>
2018-05-02 13:28:50 -0400

    Line Graphs
    Fixed reading the year from the csv file and started create_line_graph function

--- line_graphs.py
+++ line_graphs.py
@@ -9,7 +9,9 @@ def create_line_graph(ser):
         contains the names of series (as strings) that will be represented on
         the line graph
     '''
-
+    datums = []
+    for series in ser:
+        datums.append([video_game for video_game in video_games if series in video_game[0].lower()])
 
 
 f = open('videogamesales/vgsales.csv', 'r')
@@ -18,14 +20,17 @@ video_games = [] #make 2d list to store relevant data for each video game
 
 f.readline() #skip over the first line (headers)
 
-for line in f.readlines():
+for line in f.readlines():    
     #store data from each line as an array
     data = line.rstrip('\n').split(',')
     
     #get relevant data
     title = data[1]
     global_sales = float(data[10])
-    year = int(data[3])
+    try:
+        year = int(data[3])
+    except ValueError:
+        year = 'N/A'
     
     video_games.append([title, global_sales, year])
      
