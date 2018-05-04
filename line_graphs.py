df353f4100046331fb68f1096c1b1842380aa097
adrian-thumbtack <adrthamburaj@gmail.com>
2018-05-04 12:24:34 -0400

    global variables aHA

--- line_graphs.py
+++ line_graphs.py
@@ -11,12 +11,10 @@ def create_line_graph(ser):
         
         ex: ['Super Mario Galaxy', 'Fifa', 'Halo']
     '''
+    global video_games
     datums = []
     for series in ser:
         datums.append([video_game for video_game in video_games if series.lower() in video_game[0].lower()])
-    
-    print datums
-    print("\n")
     
     for datum in datums:
         titles = [title[0] for title in datum]
@@ -26,12 +24,13 @@ def create_line_graph(ser):
             if temp in titles:
                 datum[titles.index(temp)][1] += datum[i][1]
                 datum[titles.index(temp)][1] = round(datum[titles.index(temp)][1], 2)
+                datum[titles.index(temp)][2] = min([datum[titles.index(temp)][2], datum[i][2]])
                 del datum[i]
             i -= 1
-            
     print datums
-            
 
+def create_bar_graph(title):
+    pass
 
 f = open('videogamesales/vgsales.csv', 'r')
 
@@ -45,12 +44,13 @@ for line in f.readlines():
     
     #get relevant data
     title = data[1]
+    console = data[2]
     global_sales = float(data[10])
     try:
         year = int(data[3])
     except ValueError:
         year = None
     
-    video_games.append([title, global_sales, year])
+    video_games.append([title, global_sales, year, console])
      
 f.close()
